785d395c60fe5a96c19b7d99af01f8ec12302a44
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-08-22 23:06:39 +0100

    Ajax get request for recipes index and Ajax POST request for new nested comment

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,26 +1,85 @@
 $(() => {
   NewCommentSubmission();
+  NewNestedCommentSubmission();
+  RecipesOnClick();
 });
 
-const NewCommentSubmission = () => {
-   
-    $('#new-comment').on("submit", function(event) {
-      //prevent form from submitting the default way
-      event.preventDefault();
- 
-      var values = $(this).serialize();
-      var posting = $.post(`/comments`, values);
-      posting.done(function(data) {
-        const newComment = new Comment(data);
-        newComment.formatComment();
-      });
+const RecipesOnClick = () => {
+  //Event delegation
+  $(document).on('click', "#all-recipes", function(e) {
+    e.preventDefault();
+    let url = 'http://localhost:3000/recipes';
+    history.pushState(null, null, url);
+    getRecipes();
+  });
+};
+  
+const getRecipes = () => {
+  fetch(`/recipes.json`)
+  .then(response => response.json())
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      //Use constructor function to create clinic objects
+      let newRecipe = new Recipe(recipe);
+      newRecipe.formatIndex();
     });
-  };
+  });    
+};
+  
+const NewCommentSubmission = () => {
+  $(document).on('submit', "#new-comment", function(event) {
+    //prevent form from submitting the default way
+    event.preventDefault();
+    var values = $(this).serialize();
+    var posting = $.post(`/comments`, values);
+    posting.done(function(data) {
+      $('#commentResult').html('');
+      const newComment = new Comment(data);
+      newComment.formatComment();
+    });
+  });
+};
 
+const NewNestedCommentSubmission = () => {
+  $(document).on('submit', '#new-nested-comment', function(event) {
+    //prevent form from submitting the default way
+    event.preventDefault();
+    var values = $(this).serialize();
+    let recipeId = $('.recipeId').data('recipe-id');
+    var posting = $.post(`/recipes/${recipeId}/comments`, values);
+    posting.done(function(data) {
+      $('#commentResult').html('');
+      const newComment = new Comment(data);
+      newComment.formatComment();
+    });
+  });
+};
 
 //JavaScript Model Object using ES6 class
-
-
+  
+class Recipe {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.cooking_time = data.cooking_time;
+    this.servings = data.servings;
+    this.directions = data.directions;
+    this.user_id = data.user_id;
+  };
+  
+  //Prototype function (similar to instance methods) to format comment
+  formatIndex() {
+    let html = "";
+    html +=
+    `
+    <ul class="list-group list-group-flush" id="recipes-index">
+      <a href="/recipes/${this.id}" class="list-group-item"><strong>${this.name}</strong></a>
+    </ul>
+    `;
+    $("#recipe-index").append(html);
+  };
+};
+  
 class Comment {
   constructor(data) {
     this.id = data.id;
@@ -29,7 +88,7 @@ class Comment {
     this.content = data.content;
     this.userName = data.user.name;
   };
-
+  
   //Prototype function (similar to instance methods) to format comment
   formatComment() {
     let html = "";
