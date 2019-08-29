fd23ecf0ac3db06028a942a521d1072794caf4c8
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-08-29 23:39:53 +0100

    Adding userName attribute to FullRecipeSerializer

--- app/serializers/full_recipe_serializer.rb
+++ app/serializers/full_recipe_serializer.rb
@@ -1,11 +1,15 @@
 class FullRecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cooking_time, :servings, :directions
+  attributes :id, :name, :cooking_time, :servings, :directions, :userName
 
-  belongs_to :user, serializer: UserSerializer
   has_many :recipe_ingredients, each_serializer: RecipeIngredientSerializer
   has_many :ingredients, each_serializer: IngredientSerializer
   has_many :categories, each_serializer: CategorySerializer
   has_many :ratings, each_serializer: RatingSerializer
   has_many :comments, each_serializer: CommentSerializer
+
+  def userName
+    user = object.user
+    user.name
+  end
   
 end
