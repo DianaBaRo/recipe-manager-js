9b18128e86a8ff3c03e0c1ba6cae0084b7eedddb
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-08-29 23:35:26 +0100

    Adding comments, RecipeOnClick function...

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,32 +1,13 @@
+//Document ready function with arrow syntax of ES6
 $(() => {
   NewCommentSubmission();
   NewNestedCommentSubmission();
   RecipesOnClick();
+  RecipeOnClick();
 });
 
-const RecipesOnClick = () => {
-  //Event delegation
-  $(document).on('click', "#all-recipes", function(e) {
-    e.preventDefault();
-    let url = 'http://localhost:3000/recipes';
-    history.pushState(null, null, url);
-    getRecipes();
-  });
-};
-  
-const getRecipes = () => {
-  fetch(`/recipes.json`)
-  .then(response => response.json())
-  .then(recipes => {
-    recipes.forEach(recipe => {
-      //Use constructor function to create clinic objects
-      let newRecipe = new Recipe(recipe);
-      newRecipe.formatIndex();
-    });
-  });    
-};
-  
 const NewCommentSubmission = () => {
+  //Event delegation
   $(document).on('submit', "#new-comment", function(event) {
     //prevent form from submitting the default way
     event.preventDefault();
@@ -34,6 +15,7 @@ const NewCommentSubmission = () => {
     var posting = $.post(`/comments`, values);
     posting.done(function(data) {
       $('#commentResult').html('');
+      //Creating a new comment object in JS
       const newComment = new Comment(data);
       newComment.formatComment();
     });
@@ -44,16 +26,15 @@ const NewCommentSubmission = () => {
     });
   });
 };
-
+  
 const NewNestedCommentSubmission = () => {
   $(document).on('submit', '#new-nested-comment', function(event) {
-   
     //prevent form from submitting the default way
     event.preventDefault();
     var values = $(this).serialize();
     
-    let recipeId = $(this.recipe).attr('data-recipe_id');
-    // let recipeId = $('.recipeId').data('recipe-id');
+    //let recipeId = $(this.recipe).attr('data-recipe_id');
+    let recipeId = $('.recipeId').data('recipe-id');
     var posting = $.post(`/recipes/${recipeId}/comments`, values);
     posting.done(function(data) {
       $('#commentResult').html('');
@@ -67,6 +48,82 @@ const NewNestedCommentSubmission = () => {
   });
 };
 
+const RecipesOnClick = () => {
+  $(document).on('click', "#all-recipes", function(e) {
+    e.preventDefault();
+    let url = 'http://localhost:3000/recipes';
+    history.pushState(null, null, url);
+    getRecipes();
+  });
+};
+    
+const getRecipes = () => {
+  fetch(`/recipes.json`)
+  .then(response => response.json())
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      //Use constructor function to create recipe objects
+      let newRecipe = new Recipe(recipe);
+      newRecipe.formatIndex();
+    });
+  });    
+};
+
+const RecipeOnClick = () => {
+  $(document).on('click', '.show-recipe', function(e) {
+    e.preventDefault();
+    let id = $(this).attr('data-id');
+    let url = 'http://localhost:3000/recipes/' + id;
+    history.pushState(null, null, url)
+    fetch(`${id}.json`)
+    .then(response => response.json())
+    .then(recipe => {
+      let newRecipe = new Recipe(recipe);
+      newRecipe.formatShow();
+
+      const recipeComments = recipe.comments;
+      if (recipeComments === undefined || recipeComments.length == 0) {
+        $(".show-comments").remove();
+      } else {
+        recipeComments.forEach(comment => {
+          let newComment = new Comment(comment);
+          newComment.showComments();
+        });
+      };
+      
+      const recipeIngredients = recipe.recipe_ingredients;
+      if (recipeIngredients === undefined || recipeIngredients.length == 0) {
+        $(".show-ingredients").remove();
+      } else {
+        recipeIngredients.forEach(ri => {
+          let newRecipeIngredient = new RecipeIngredient(ri);
+          newRecipeIngredient.showIngredients();
+        });
+      };
+
+      const recipeRatings = recipe.ratings;
+      if (recipeRatings === undefined || recipeRatings.length == 0) {
+        $(".show-ratings").remove();
+      } else {
+        recipeRatings.forEach(rating => {
+          let newRating = new Rating(rating);
+          newRating.showRatings();
+        });
+      };
+
+      const recipeCategories = recipe.categories;
+      if (recipeCategories === undefined || recipeCategories.length == 0) {
+        $(".show-categories").remove();
+      } else {
+        recipeCategories.forEach(category => {
+          let newCategory = new Category(category);
+          newCategory.showCategories();
+        });
+      };
+    });
+  });
+};
+
 //JavaScript Model Object using ES6 class
   
 class Recipe {
@@ -76,7 +133,7 @@ class Recipe {
     this.cooking_time = data.cooking_time;
     this.servings = data.servings;
     this.directions = data.directions;
-    this.user_id = data.user_id;
+    this.userName = data.userName
   };
   
   //Prototype function (similar to instance methods) to format comment
@@ -84,11 +141,47 @@ class Recipe {
     let html = "";
     html +=
     `
-    <ul class="list-group list-group-flush" id="recipes-index">
-      <a href="/recipes/${this.id}" class="list-group-item"><strong>${this.name}</strong></a>
+    <ul class="list-group list-group-flush">
+      <a href="/recipes/${this.id}" data-id="${this.id}" class="list-group-item show-recipe"><strong>${this.name}</strong></a>
     </ul>
     `;
     $("#recipe-index").append(html);
+  };
+
+  formatShow() {
+    let html = "";
+    //Template literal (Template string)
+    html +=
+    `
+    <div class="container text-center">
+      <h1>${this.name}</h1>
+      <br />
+    </div>
+    <div class="container">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cooking time in minutes: ${this.cooking_time}</li>
+        <li class="list-group-item">Servings: ${this.servings}</li>
+        <li class="list-group-item">Directions: ${this.directions}</li>
+        <li class="list-group-item show-ingredients">Ingredients:</li>
+        <li class="list-group-item show-categories">Categories:</li>
+        <li class="list-group-item"><strong>Recipe created by ${this.userName}</strong></li>
+      </ul>
+    </div>
+    <br /><br />
+    <div class="show-ratings">
+      <h6>Ratings:</h6><br />
+    </div>
+    <br /><br />
+    <div class="btn btn-primary">
+      <a href="/recipes/${this.id}/comments/new" class="btn text-white">Comment recipe</a>
+    </div>
+    <br /><br />
+    <div class="show-comments">
+      <h6>Comments:</h6><br />
+    </div>
+    `;
+    $("#recipe-index").html('');
+    $("#recipe-show").html(html);
   };
 };
   
@@ -96,12 +189,11 @@ class Comment {
   constructor(data) {
     this.id = data.id;
     this.user_id = data.user_id;
-    this.recipe_id = data.recipe_id;
+    this.recipeId = data.recipeId;
     this.content = data.content;
-    this.userName = data.user.name;
+    this.commentUser = data.commentUser;
   };
   
-  //Prototype function (similar to instance methods) to format comment
   formatComment() {
     let html = "";
     html +=
@@ -109,9 +201,74 @@ class Comment {
     <div class="container text-center">
       <h4>Comment created succesfully</h4><br />
       <p>${this.content}</p><br />
-      <p><strong>Posted by ${this.userName}</strong></p>
+      <p><strong>Posted by ${this.commentUser}</strong></p>
+      <p><a href="/recipes/${this.recipeId}">[ Back to recipe ]</a></p
     </div>
     `;
     $("#commentResult").append(html);
+  };
+
+  showComments() {
+    let html = "";
+    html +=
+    `
+    <p>${this.content}
+    <strong>Posted by ${this.commentUser}</strong><br /></p>
+    `;
+    $("#recipe-index").html('');
+    $(".show-comments").append(html);
+  };
+};
+
+class RecipeIngredient {
+  constructor(data) {
+    this.id = data.id;
+    this.quantity = data.quantity;
+    this.ingredientName = data.ingredientName
+  };
+  
+  showIngredients() {
+    let html = "";
+    html +=
+    `
+    ${this.ingredientName} (${this.quantity}) 
+    `;
+    $("#recipe-index").html('');
+    $(".show-ingredients").append(html);
+  };
+};
+
+class Rating {
+  constructor(data) {
+    this.id = data.id;
+    this.score = data.score;
+    this.ratingUser = data.ratingUser
+  };
+  
+  showRatings() {
+    let html = "";
+    html +=
+    `
+    <p>${this.score} star - <strong> ${this.ratingUser}</strong><br /></p>
+    `;
+    $("#recipe-index").html('');
+    $(".show-ratings").append(html);
+  };
+};
+
+class Category {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+  };
+    
+  showCategories() {
+    let html = "";
+    html +=
+    `
+    ${this.name}
+    `;
+    $("#recipe-index").html('');
+    $(".show-categories").append(html);
   };
 };
