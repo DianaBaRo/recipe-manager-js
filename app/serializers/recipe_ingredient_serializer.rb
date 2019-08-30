6863e4fd6216f21dd6f95dd3ecda2958dfbfb5b6
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-08-30 14:19:00 +0100

    Adding comment

--- app/serializers/recipe_ingredient_serializer.rb
+++ app/serializers/recipe_ingredient_serializer.rb
@@ -1,6 +1,7 @@
 class RecipeIngredientSerializer < ActiveModel::Serializer
   attributes :id, :quantity, :ingredientName
 
+  #With this method I don't need the ingredient serializer
   def ingredientName
     ingredient = object.ingredient
     ingredient.name
