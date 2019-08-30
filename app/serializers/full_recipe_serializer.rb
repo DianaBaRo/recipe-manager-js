31b44600437f220c87b716e4c4e732541b5fc09f
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-08-30 14:17:43 +0100

    Delete has_many ingredients relation

--- app/serializers/full_recipe_serializer.rb
+++ app/serializers/full_recipe_serializer.rb
@@ -2,7 +2,6 @@ class FullRecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :cooking_time, :servings, :directions, :userName
 
   has_many :recipe_ingredients, each_serializer: RecipeIngredientSerializer
-  has_many :ingredients, each_serializer: IngredientSerializer
   has_many :categories, each_serializer: CategorySerializer
   has_many :ratings, each_serializer: RatingSerializer
   has_many :comments, each_serializer: CommentSerializer
